ee36c243407760621011acc30ac47347778482ab
anchulamsc <anchu.venki@gmail.com>
2025-09-06 19:36:10 -0500

    App name changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.anchoos.app',
+  appId: 'com.anchoos.nutrical',
   appName: 'nutrical',
   webDir: 'dist'
 };
